f030af3413e6175ab447bbe661bb3c64ec950792
kh4leoni <73070339+kh4leoni@users.noreply.github.com>
2023-03-12 12:28:21 +0200

    Lisätty ympäristömuuttuja porttiin

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.post('/api/notes', (req, res) => {
   
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(3001, () => {
     console.log(`Server is running on port ${PORT}`)
